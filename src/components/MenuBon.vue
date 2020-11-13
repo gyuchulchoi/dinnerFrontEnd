710e351e1983a91d703e49a7ba8acf632e510306
DESKTOP-KKKJEGF\kds <dd@dd.cd>
2020-11-13 15:38:55 +0900

    에러수정

--- src/components/MenuBon.vue
+++ src/components/MenuBon.vue
@@ -2,16 +2,16 @@
 <b-card v-if="menu" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
     <b-card-title>
         <div>
-            본도시락 : {{menu.menu_name}}
+            {{menu.menu_name}}
         </div>
     </b-card-title>
     <b-card-footer>
-        {{menu.menu_price}}원
+        {{menu.menu_price | comma}}원
     </b-card-footer>
     <div>
         <b-button v-b-modal="menu.id + 'id'">주문표에 추가하기</b-button>
 
-        <b-modal :id="menu.id + 'id'" title="BootstrapVue" @ok="onClickButton(menu.id, orderer)">
+        <b-modal :id="menu.id + 'id'" title="추가하기" @ok="insertMenu(menu, orderer)">
             <b-form-input placeholder="먹을사람 이름을 입력하세요" v-model="orderer" />
             <p class="my-4">{{menu.menu_name}}</p>
             <p class="my-4">{{menu.menu_price}}</p>
@@ -25,6 +25,11 @@
 import {
     utils
 } from '../assets/mixin'
+import axios from 'axios';
+import {
+    API_SERVER,
+    ORDERS
+} from '../assets/urls';
 
 export default {
     mixins: [utils('addOrderEvent')],
@@ -39,11 +44,22 @@ export default {
         }
     },
     methods: {
-        onClickButton: function (menu, orderer) {
+        insertMenu: function (menu, orderer) {
+            axios.post(API_SERVER + ORDERS, {
+                    name: menu.menu_name,
+                    price: menu.menu_price,
+                    orderer,
+                    menu_type: 'bon'
+                })
+                .then(res => {
+                    this.noticeAddOrder(res.data)
+                }, err => {
+                    console.log(err)
+                })
+        },
+        noticeAddOrder: function (order) {
             this.$socket.emit(this.addOrderEvent, {
-                ...menu,
-                orderer,
-                menu_type: 'bon'
+                order
             })
         }
     }
